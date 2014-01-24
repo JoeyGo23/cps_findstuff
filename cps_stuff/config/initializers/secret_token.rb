3fd568daabcf56af696e6cb61cac8cfd238efe0b
Joe G <wheeltirefamily@gmail.com>
2014-01-24 14:44:05 -0600

    init app

---
+++ cps_stuff/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CpsStuff::Application.config.secret_key_base = '408e4ac86b773c96a1789e9e1b21b4fa839083a2f60ef018f8cd08af6c2637ccd10b9c23205567759e41dae2ee9aae41c56b9b58de31b2ad028f40a6dbccff56'
